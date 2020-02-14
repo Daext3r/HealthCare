24b2ec3921175df3f8a29fe8874e38ab111398d0
Daext3r <daext3r@gmail.com>
2020-02-14 15:24:58 +0100

    Primer commit

---
+++ application/views/Inicio_v.php
@@ -0,0 +1,43 @@
+<body>
+    <!--
+        Estilos: assets/inicio/estilos.css
+        Script: assets/inicio/script.js
+    -->
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/inicio/estilos.css">
+
+    <div class="row shadow p-3 mb-5 bg-white rounded">
+        <div class="mini-row">
+            <img src="<?php echo base_url(); ?>assets/img/logo.png" alt="logo">
+        </div>
+        <div class="mini-row">
+            <h5>Desarrollado por Alejandro D. Consulte licencia de uso.</h5>
+        </div>
+        <div class="mini-row">
+            <a href="#info" class="btn btn-outline-info">Más información</a>
+            <a href="<?php echo base_url()?>login" class="btn btn-outline-success">Iniciar sesión</a>
+        </div>
+    </div>
+
+    <div class="row shadow p-3 mb-5 bg-white rounded" id="info">
+        <div class="col-8">1</div>
+        <div class="col-4">
+            <h2>Simplicidad</h2>
+            <h4>Diseño simple e intuitivo que te evitará dolores de cabeza, perfecto para los más mayores.</h4>
+        </div>
+    </div>
+    <div class="row shadow p-3 mb-5 bg-white rounded">
+        <div class="col-4">
+            <h2>Tratamientos</h2>
+            <h4>Obtén información de tus tratamientos en dos sencillos pasos.</h4>
+        </div>
+        <div class="col-8">1</div>
+
+    </div>
+    <div class="row shadow p-3 mb-5 bg-white rounded">
+        <div class="col-8">1</div>
+        <div class="col-4">
+            <h2>Citas</h2>
+            <h4>Consulta tus citas, solicita una nueva, o anula alguna a la que no puedas acudir</h4>
+        </div>
+    </div>
+</body>
