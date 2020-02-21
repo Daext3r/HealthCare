3b2c85ca8e3e2c52c6b9b65093743721be5be870
Daext3r <daext3r@gmail.com>
2020-02-21 10:59:54 +0100

    Modificado responsive del logo de login

--- application/views/Login_v.php
+++ application/views/Login_v.php
@@ -1,7 +1,7 @@
 <body>
     <div class="row shadow-lg p-3 mb-5 bg-white rounded">
 
-        <img src="<?php echo base_url() ?>assets/img/logo.png" alt="" srcset="" class="img-fluid">
+        <img src="<?php echo base_url() ?>assets/img/logo.png" alt="" srcset="" class="col-12 col-md-8 col-l-7 col-xl-5">
 
         <form method="POST" action="<?php echo base_url() ?>login/autenticar" class="col-md-12 col-xl-7">
             <?php if ($this->session->flashdata('error') == 'no_user') { ?>
