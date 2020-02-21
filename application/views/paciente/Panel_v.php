61fbc2fd0bc9db721703894e1fdccc970c2e1c04
Daext3r <daext3r@gmail.com>
2020-02-21 14:09:53 +0100

    Modificado panel del paciente

--- application/views/paciente/Panel_v.php
+++ application/views/paciente/Panel_v.php
@@ -9,7 +9,7 @@
         <nav id="header">
             <div class="headerItem"><?php echo $this->session->userdata("nombre") . " " . $this->session->userdata("apellidos")?></div>
             <div class="headerItem" id="reloj"></div>
-            <div class="headerItem">Tienes 0 notificaciones pendientes</div>
+            <div class="headerItem hover" id="notificaciones">Tienes <span id="notificaciones" class="badge badge-pill badge-danger">0</span> notificaciones pendientes</div>
         </nav>
 
         <section id="contenido">
--- assets/scripts/paciente.js
+++ assets/scripts/paciente.js
@@ -1,26 +1,38 @@
-$(document).ready(function(){
-        
+$(document).ready(function () {
+
     let dias = new Array("lunes", "martes", "miércoles", "jueves", "viernes", "sabado", "domingo");
-    let meses = new Array ("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");
+    let meses = new Array("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre");
 
     function actualizarFecha() {
-    let d = new Date();
+        let d = new Date();
         let cadena = "Hoy es ";
         cadena += dias[d.getUTCDay() - 1] + " ";
         cadena += d.getUTCDate() + " de ";
-        
+
         cadena += meses[d.getUTCMonth()] + " de ";
         cadena += d.getUTCFullYear();
 
         cadena += " | ";
 
         cadena += d.getHours() + ":";
-        cadena += d.getMinutes();
+
+        //si los minutos son de 0 a 9
+        if (d.getMinutes().toString().length == 1) {
+            //añade un 0 delante
+            cadena += "0" + d.getMinutes();
+        } else {
+            //de lo contrario pone la fecha
+            cadena += d.getMinutes();
+        }
         $("#reloj").text(cadena);
-}
+    }
     //ejecutamos la funcion de la fecha una vez
-    actualizarFecha();      
+    actualizarFecha();
 
     //intervalo de ejecucion del reloj
     setInterval(actualizarFecha, 10000);
+
+    $("#notificaciones").on("click", () => {
+        //TODO: mostrar listado con notificaciones
+    })
 });
