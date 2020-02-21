931f4f60bb6ec3ac6d5213e6f99d6e3dc44dec17
Daext3r <daext3r@gmail.com>
2020-02-21 09:19:16 +0100

    El inicio ahora es responsive

--- application/views/Inicio_v.php
+++ application/views/Inicio_v.php
@@ -1,7 +1,7 @@
 <body>
-    <div class="row shadow p-3 mb-5 bg-white rounded">
+    <div class="row shadow p-3 mb-5 bg-white rounded col-10">
         <div class="mini-row">
-            <img src="<?php echo base_url(); ?>assets/img/logo.png" alt="logo">
+            <img src="<?php echo base_url(); ?>assets/img/logo.png" alt="logo" class="img-fluid">
         </div>
         <div class="mini-row">
             <h5>Desarrollado por Alejandro D. Consulte licencia de uso.</h5>
@@ -12,24 +12,24 @@
         </div>
     </div>
 
-    <div class="row shadow p-3 mb-5 bg-white rounded" id="info">
-        <div class="col-8">1</div>
-        <div class="col-4">
+    <div class="row shadow p-3 mb-5 bg-white rounded col-10" id="info">
+        <div class="col-lg-8">1</div>
+        <div class="col-lg-4">
             <h2>Simplicidad</h2>
             <h4>Diseño simple e intuitivo que te evitará dolores de cabeza, perfecto para los más mayores.</h4>
         </div>
     </div>
-    <div class="row shadow p-3 mb-5 bg-white rounded">
-        <div class="col-4">
+    <div class="row shadow p-3 mb-5 bg-white rounded col-10">
+        <div class="col-lg-4">
             <h2>Tratamientos</h2>
             <h4>Obtén información de tus tratamientos en dos sencillos pasos.</h4>
         </div>
-        <div class="col-8">1</div>
+        <div class="col-lg-8">1</div>
 
     </div>
-    <div class="row shadow p-3 mb-5 bg-white rounded">
-        <div class="col-8">1</div>
-        <div class="col-4">
+    <div class="row shadow p-3 mb-5 bg-white rounded col-10">
+        <div class="col-lg-8">1</div>
+        <div class="col-lg-4">
             <h2>Citas</h2>
             <h4>Consulta tus citas, solicita una nueva, o anula alguna a la que no puedas acudir</h4>
         </div>
