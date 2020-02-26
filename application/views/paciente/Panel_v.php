5cd16a1a6c29f6311cb8205840935c722b1ab761
Daext3r <daext3r@gmail.com>
2020-02-26 09:44:19 +0100

    Creado diseño del menu lateral

--- application/views/paciente/Panel_v.php
+++ application/views/paciente/Panel_v.php
@@ -1,13 +1,27 @@
 <body>
     <!--seccion del menu lateral-->
     <section id="menu">
+        <div id="perfil">
+            <div id="imgperfil">
+                <img src="https://www.jennstrends.com/wp-content/uploads/2013/10/bad-profile-pic-2-768x768.jpeg" alt="">
+            </div>
 
+            <div id="nombre">
+                <?php echo $this->session->userdata("nombre") . " " . $this->session->userdata("apellidos") ?>
+            </div>
+
+            <div class="separador"></div>
+        </div>
+        
+        <div id="secciones">
+
+        </div>
     </section>
 
     <!--seccion del contenido-->
     <div id="pagina">
         <nav id="header">
-            <div class="headerItem"><?php echo $this->session->userdata("nombre") . " " . $this->session->userdata("apellidos") ?></div>
+            <div class="headerItem"></div>
             <div class="headerItem" id="reloj"></div>
             <div class="headerItem hover" id="notificaciones">Tienes <span id="notificaciones" class="badge badge-pill badge-danger">0</span> notificaciones pendientes</div>
             <div id="logout"><i class="fas fa-sign-out-alt"></i>
@@ -16,7 +30,7 @@
         </nav>
 
         <section id="contenido">
-            c
+
         </section>
     </div>
 
@@ -24,7 +38,7 @@
     <script>
         $("#logout").on("click", () => {
             //si hace clic en el boton de logout, redirigimos al login
-            window.location = "<?php echo base_url()?>" + "paciente/logout";
+            window.location = "<?php echo base_url() ?>" + "paciente/logout";
         });
     </script>
 </body>
