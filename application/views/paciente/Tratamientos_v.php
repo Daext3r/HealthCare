05cf693bc2db1912ecf50c1fcaf9a91b69024a66
Daext3r <daext3r@gmail.com>
2020-03-04 23:02:43 +0100

    Cargada info del medicamento desde la API

--- application/views/paciente/Tratamientos_v.php
+++ application/views/paciente/Tratamientos_v.php
@@ -1,17 +1,5 @@
         <section id="tratamientos-contenido">
-            <table class="table table-bordered">
-                <tr class="table-primary ">
-                    <th scope="col">Medicamento</th>
-                    <th scope="col">Desde</th>
-                    <th scope="col">Hasta</th>
-                    <th scope="col">Dosis</th>
-                </tr>
-                <tr>
-                    <td colspan="4" class="bg-white">
-                        <h5>No tienes tratamientos activos</h5>
-                    </td>
-                </tr>
-            </table>
+            <ul id="lista"></ul>
         </section>
         </div>
         </body>
--- assets/scripts/paciente/tratamientos.js
+++ assets/scripts/paciente/tratamientos.js
@@ -2,12 +2,56 @@ $(document).ready(function () {
     //mostramos la cantidad de notificaciones
     $("#notificaciones").text(localStorage.getItem("notificaciones"));
 
-    $.post(localStorage.getItem("hc_base_url") + "Tratamientos_controller/leerTratamientos", {}, function(data) {
+    $.post(localStorage.getItem("hc_base_url") + "Tratamientos_controller/leerTratamientos", {}, function (data) {
         data = JSON.parse(data);
-
+        console.log(data);
         //si no hay datos, no ejecutamos nada
-        if(data.length == 0) return;
-        
-             
+        if (data.length == 0) return;
+
+        //borramos el contenido del cuerpo
+        $("#tratamientos").html("");
+
+
+        for (let tratamiento of data) {
+
+
+            //primero leemos el nombre del medicamento
+            fetch(`https://cima.aemps.es/cima/rest/medicamento?nregistro=${tratamiento.nregistro}`)
+                .then(function (respuesta) {
+                    //convertimos la respuesta en un objeto literal json
+                    return respuesta.json();
+                })
+                .then(function (respuesta) {
+                    //creamos un elemento td y lo almacenamos
+                    let enlace = document.createElement("a");
+                    enlace.innerText = respuesta.nombre;
+                    enlace.href = "";
+
+                    let li = document.createElement("li");
+                    $(enlace).appendTo(li);
+                    $(li).appendTo("#lista");
+
+                    //VENTANA POPUP
+                    $(enlace).on("click", function (e) {
+                        e.preventDefault();
+                        Swal.fire({
+                            title: 'Tratamiento',
+                            html:
+                                `Fecha de inicio del tratamiento ${new Date(tratamiento.fecha_inicio).toLocaleDateString()}<br>
+                                Fecha de fin del tratamiento ${new Date(tratamiento.fecha_fin).toLocaleDateString()}<br>`,
+                                //hacer bucle for para las horas en un ul
+                            showCloseButton: true,
+                            showCancelButton: false,
+                            focusConfirm: true,
+                            confirmButtonText: "Cerrar",
+                            
+                            cancelButtonAriaLabel: 'Thumbs down'
+                        })
+                    })
+                });
+
+
+        }
+
     })
 });
