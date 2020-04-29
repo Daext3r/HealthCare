d603e8f433b4e7815d9dbdf3eca7f44c56652fc5
Daext3r <daext3r@gmail.com>
2020-04-29 17:44:41 +0200

    Vista de citas

---
+++ application/views/facultativo/View_Citas.php
@@ -0,0 +1,19 @@
+<section class="contenido">
+
+   <table class="table">
+      <thead class="table-primary">
+         <tr>
+            <th scope="col">Paciente</th>
+            <th scope="col">Hora</th>
+            <th scope="col">Estado</th>
+            <th scope="col">Atender</th>
+            <th scope="col">Actualizar</th>
+         </tr>
+      </thead>
+      <tbody id="tabla">
+      </tbody>
+   </table>
+
+</section>
+</div>
+</body>
