f777aa9730c005efb0dfe85bcb40b3d89ecb7898
Daext3r <daext3r@gmail.com>
2020-05-06 15:06:32 +0200

    Espaciado de archivo

--- assets/scripts/modules/ScriptModule_Panel.js
+++ assets/scripts/modules/ScriptModule_Panel.js
@@ -1,11 +1,11 @@
 //este archivo se cargará en gran parte de la aplicacion ya que es ampliamente usado
 $(document).ready(function () {
 
-   $("a").click(function(e) {
+   $("a").click(function (e) {
       e.preventDefault();
       $("section.contenido").eq(0).fadeOut(200);
-      setTimeout(() => {window.location.href = this.href;}, 200);
-      
+      setTimeout(() => { window.location.href = this.href; }, 200);
+
    });
 
    $("section.contenido").eq(0).fadeIn(200);
@@ -18,7 +18,7 @@ $(document).ready(function () {
       let d = new Date();
       //formamos la cadena del reloj
       let cadena = `Hoy es ${dias[d.getUTCDay()]} ${d.getUTCDate()} de ${meses[d.getUTCMonth()]} de ${d.getUTCFullYear()} | 
-      ${d.getHours()}:${d.getMinutes().toString().length == 1 ? "0":""}${d.getMinutes()}`;
+      ${d.getHours()}:${d.getMinutes().toString().length == 1 ? "0" : ""}${d.getMinutes()}`;
 
       //la mostramos 
       $("#reloj").text(cadena);
@@ -47,58 +47,58 @@ $(document).ready(function () {
       window.location = localStorage.getItem("hc_base_url") + "login";
    });
 
-      //cuando el documento se haya cargado, leemos las notificaciones
-      $.post(localStorage.getItem("hc_base_url") + "Usuarios_controller/leerNotificaciones", {}, (data) => {
-         //convertimos a JSON
-         data = JSON.parse(data);
-   
-         //guardamos en el localstorage la cantidad de notificaciones
-         localStorage.setItem("notificaciones", data.length);
-   
-         //mostramos la cantidad de notificaciones
-         $("#notificaciones").text(localStorage.getItem("notificaciones"));
-   
-         for (let notificacion of data) {
-            //creamos la notificacion con todos los datos correspondientes
-            let notif = document.createElement("a");
-            notif.innerText = notificacion.resumen;
-            notif.href = "";
-            notif.dataset.id = notificacion.id;
-   
-            $(notif).appendTo($("#listaNotificaciones"));
-   
-            //añadimos un event listener al elemento
-            $(notif).on("click", function (e) {
-               //evitamos que nos redirija 
-               e.preventDefault();
-   
-               Swal.fire(
-                  {
-                     'confirmButtonText': "Cerrar",
-                     'icon': 'warning',
-                     'title': `${notificacion.resumen}`,
-                     'text': `${notificacion.informacion}`,
-                     'onClose': (event) => {
-                        //cuando cierra la notificacion es que ya la ha leido
-                        //mandamos borrarla de la bbdd
-   
-                        $.post(localStorage.getItem("hc_base_url") + "Usuarios_controller/borrarNotificacion", { id: notificacion.id }, (data) => {
-   
-                           //reducimos la cantidad de notificaciones en uno en el localstorage
-                           localStorage.setItem("notificaciones", parseInt(localStorage.getItem("notificaciones")) - 1);
-   
-                           //cuando se haya completado, actualizamos los contadores
-                           $("#notificaciones").text(localStorage.getItem("notificaciones"));
-   
-                           $("#cantidad-notificaciones").text(localStorage.getItem("notificaciones"));
-   
-                           //this es el enlace al que hacemos clic, puesto que despues de el enlace usamos funciones lambda para no generar un nuevo this
-                           $(this).remove();
-                        });
-                     }
+   //cuando el documento se haya cargado, leemos las notificaciones
+   $.post(localStorage.getItem("hc_base_url") + "Usuarios_controller/leerNotificaciones", {}, (data) => {
+      //convertimos a JSON
+      data = JSON.parse(data);
+
+      //guardamos en el localstorage la cantidad de notificaciones
+      localStorage.setItem("notificaciones", data.length);
+
+      //mostramos la cantidad de notificaciones
+      $("#notificaciones").text(localStorage.getItem("notificaciones"));
+
+      for (let notificacion of data) {
+         //creamos la notificacion con todos los datos correspondientes
+         let notif = document.createElement("a");
+         notif.innerText = notificacion.resumen;
+         notif.href = "";
+         notif.dataset.id = notificacion.id;
+
+         $(notif).appendTo($("#listaNotificaciones"));
+
+         //añadimos un event listener al elemento
+         $(notif).on("click", function (e) {
+            //evitamos que nos redirija 
+            e.preventDefault();
+
+            Swal.fire(
+               {
+                  'confirmButtonText': "Cerrar",
+                  'icon': 'warning',
+                  'title': `${notificacion.resumen}`,
+                  'text': `${notificacion.informacion}`,
+                  'onClose': (event) => {
+                     //cuando cierra la notificacion es que ya la ha leido
+                     //mandamos borrarla de la bbdd
+
+                     $.post(localStorage.getItem("hc_base_url") + "Usuarios_controller/borrarNotificacion", { id: notificacion.id }, (data) => {
+
+                        //reducimos la cantidad de notificaciones en uno en el localstorage
+                        localStorage.setItem("notificaciones", parseInt(localStorage.getItem("notificaciones")) - 1);
+
+                        //cuando se haya completado, actualizamos los contadores
+                        $("#notificaciones").text(localStorage.getItem("notificaciones"));
+
+                        $("#cantidad-notificaciones").text(localStorage.getItem("notificaciones"));
+
+                        //this es el enlace al que hacemos clic, puesto que despues de el enlace usamos funciones lambda para no generar un nuevo this
+                        $(this).remove();
+                     });
                   }
-               )
-            });
-         }
-      });
+               }
+            )
+         });
+      }
+   });
 });
