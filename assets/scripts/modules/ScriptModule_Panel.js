a668d9a859362ce6d9ae1a49ba930ec7682a434f
Daext3r <daext3r@gmail.com>
2020-05-29 17:53:40 +0200

    fix

--- assets/scripts/modules/ScriptModule_Panel.js
+++ assets/scripts/modules/ScriptModule_Panel.js
@@ -202,7 +202,7 @@ $(document).ready(function () {
       });
    });
 
-   $("#toggle-menu").children().eq(0).click(() => {
+   $("#toggle-menu").click(() => {
       //añadimos al body una clase que ocultara el menu
       document.getElementsByTagName("body")[0].classList.toggle("toggle-menu");
    })
